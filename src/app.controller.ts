6630dfb90e4334e0ce400894770140543992b720
ParkChaerim <qkrdbsk28@naver.com>
2022-06-20 16:44:01 +0900

    service 개념

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,11 +7,12 @@ export class AppController {
 
   @Get() // express의 get 라우터와 같은 역할
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello(); // 여기서 서비스가 필요한 이유는?
+    // NestJS는 controller를 비지니스 로직과 구분 짓고 싶어한다 (controller는 그저 url을 가져오는 역할일 뿐..)
   }
 
-  @Get('/hello') // 데코레이터는 반드시 꾸며주는 함수 바로 위에 있어야 잘 작동한다.
+  @Get('/hi') // 데코레이터는 반드시 꾸며주는 함수 바로 위에 있어야 잘 작동한다.
   sayHello(): string {
-    return 'Hello everyone';
+    return this.appService.getHi();
   }
 }
