a8d046495cdf58338a2cce43abc3e6511a5e086b
ParkChaerim <qkrdbsk28@naver.com>
2022-06-21 13:20:56 +0900

    Decorator

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Post, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Patch,
+  Body,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies') // 127.0.0.1:3000/movies url로 이어지는 컨트롤러
 export class MoviesController {
@@ -7,24 +16,32 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
-  @Get('/:id')
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie made after: ${searchingYear}`;
+  }
+
+  @Get(':id')
   getOne(@Param('id') movieId: string) {
     // 파라미터로 넘어온 id 값을 알고싶다면? nest 에서는 무언가가 필요하면 요청을 해야한다 -> @Param decorator 사용
     return `This will return one movie with the id: ${movieId}`;
   }
 
   @Post()
-  create() {
-    return 'This will create a movie';
+  create(@Body() movieData) {
+    return movieData;
   }
 
-  @Delete('/:id')
+  @Delete(':id')
   remove(@Param('id') movieId: string) {
     return `This will delete a movie with the id: ${movieId}`;
   }
 
-  @Patch('/:id') // Put은 모두 업데이트 Patch는 일부 리소스만 업데이트
-  patch(@Param('id') movieId: string) {
-    return `This will patch a movie with the id: ${movieId}`;
+  @Patch(':id') // Put은 모두 업데이트 Patch는 일부 리소스만 업데이트
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMoive: movieId,
+      ...updateData,
+    };
   }
 }
