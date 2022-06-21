0d8ac6dc91f367120dfef81f0578e301633c58de
ParkChaerim <qkrdbsk28@naver.com>
2022-06-21 12:57:12 +0900

    movie controller 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
 
 // 데코레이터 -> 클래스에 함수 기능을 추가할 수 있다.
 @Module({
   imports: [],
-  controllers: [AppController], // controller는 기본적으로 url을 가져오고 함수를 실행하는 일을 한다. -> express의 라우터 같은 존재
-  providers: [AppService],
+  controllers: [MoviesController], // controller는 기본적으로 url을 가져오고 함수를 실행하는 일을 한다. -> express의 라우터 같은 존재
+  providers: [],
 })
 export class AppModule {}
---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Delete, Get, Param, Post, Patch } from '@nestjs/common';
+
+@Controller('movies') // 127.0.0.1:3000/movies url로 이어지는 컨트롤러
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'This will return all movies';
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: string) {
+    // 파라미터로 넘어온 id 값을 알고싶다면? nest 에서는 무언가가 필요하면 요청을 해야한다 -> @Param decorator 사용
+    return `This will return one movie with the id: ${movieId}`;
+  }
+
+  @Post()
+  create() {
+    return 'This will create a movie';
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie with the id: ${movieId}`;
+  }
+
+  @Patch('/:id') // Put은 모두 업데이트 Patch는 일부 리소스만 업데이트
+  patch(@Param('id') movieId: string) {
+    return `This will patch a movie with the id: ${movieId}`;
+  }
+}
