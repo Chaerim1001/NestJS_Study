7fd668f4ff9cd54fbc3a385a6e729031f4e3d88f
ParkChaerim <qkrdbsk28@naver.com>
2022-06-22 16:08:11 +0900

    [e2e Testing] movies

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -16,9 +16,32 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
+    // url에 대한 요청에 대한 테스트
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('welcome to my movie API');
+  });
+
+  describe('/movies', () => {
+    it('GET', () => {
+      // 해당 프로젝트에서 사용하는 가짜 데이터베이스는 시작이 항상 비어있으니까 expect([])
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+    });
+
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'TEST',
+          year: 2022,
+          genres: ['TEST'],
+        })
+        .expect(201);
+    });
+
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
   });
 });
