3317d0ab6b4768570d0eb327adfbc854165c48e9
ParkChaerim <qkrdbsk28@naver.com>
2022-06-23 09:28:35 +0900

    [e2e Testing] movies id GET

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  // beforeEach로 세팅을 해놓으면 테스트마다 (테스트를 위한) 어플리케이션을 생성하게 된다.
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
+    // ***** e2e 테스트를 할 때에느 테스트에도 실제 어플리케이션의 환경을 그대로 적용시켜줘야 한다.
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -30,7 +38,7 @@ describe('AppController (e2e)', () => {
     });
 
     it('POST', () => {
-      return request(app.getHttpServer())
+      return request(app.getHttpServer()) // 127.0.0.1:3000 이런식으로 쓰는걸 막기 위해
         .post('/movies')
         .send({
           title: 'TEST',
@@ -43,5 +51,18 @@ describe('AppController (e2e)', () => {
     it('DELETE', () => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
     });
+  });
+
+  describe('/movies/:id', () => {
+    //GET, DELETE, PATCH 모두 테스트 -> it.todo() 사용
+    // it.todo('GET');
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
+    it.todo('DELETE');
+    it.todo('PATCH');
   });
 });
